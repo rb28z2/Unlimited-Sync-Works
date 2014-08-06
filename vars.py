efbd71eb3fdb3adf05605e641f33518092526dca
Smoothtalk <Smoothtalk@users.noreply.github.com>
2014-08-05 22:10:06 -0600

    fixed vars

--- vars.py
+++ vars.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+#It is crucial that you have a '/' after your directory
+
+#"/home/test/downloads/" <- Good
+
+#"/home/test/downloads" <- Bad
+
 down_dir = "/home/USERNAME/downloads/"
 a_host = "USER@HOSTNAME"
 k_host "USER2@HOSTNAME2"
